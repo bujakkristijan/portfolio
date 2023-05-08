54ccf8b9868686d58968f5a3d65f6e209730cd34
Kristijan Bujak <cileb411@gmail.com>
2023-05-09 00:11:27 +0200

    experience komponenta uradjena, dodate fontawesome ikonice za svaku tehnologiju posebno

--- portfolio/src/components/experience/ExperienceComponent.js
+++ portfolio/src/components/experience/ExperienceComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDatabase  } from '@fortawesome/free-solid-svg-icons'
-import { faHtml5, faJs, faCss3, faAngular, faReact, faJava, faGithub, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faJs, faCss3, faAngular, faReact, faJava, faGithub, faPython, faAndroid } from '@fortawesome/free-brands-svg-icons';
 import './ExperienceComponent.css'
 
 const ExperienceComponent = () => {
@@ -10,22 +10,91 @@ const ExperienceComponent = () => {
         <h1 className='experience-title'>
             Experience
         </h1>
-        <div className='experience-details-container'>
-            <div className='about-containers'>
+        
+            <div className='experience-details-container'>
                 <div className='details-containers'>
                     <h2 className='experience-sub-title'>Frontend development</h2>
+                    <div className='article-main-container'>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faHtml5} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>HTML</div>
+                                <p className='article-technology-level'>Intermediate </p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faCss3} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>CSS</div>
+                                <p className='article-technology-level'>Intermediate</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faJs} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>JavaScript</div>
+                                <p className='article-technology-level'>Experienced</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faReact} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>React</div>
+                                <p className='article-technology-level'>Experienced</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faAngular} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>Angular</div>
+                                <p className='article-technology-level'>Intermediate </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='details-containers'>
+                    <h2 className='experience-sub-title'>Backend development</h2>
+                    <div className='article-main-container'>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faJava} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>Java Spring</div>
+                                <p className='article-technology-level'>Experienced</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faDatabase} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>MySql</div>
+                                <p className='article-technology-level'>Intermediate</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faGithub} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>Github</div>
+                                <p className='article-technology-level'>Intermediate</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faPython} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>Python</div>
+                                <p className='article-technology-level'>Basic</p>
+                            </div>
+                        </div>
+                        <div className='article-container'>
+                            <FontAwesomeIcon className="fa-icon" icon={faAndroid} />
+                            <div className='article-details-container'>
+                                <div className='article-techology-title'>Android Studio</div>
+                                <p className='article-technology-level'>Basic</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <FontAwesomeIcon icon={faHtml5} />
-        <FontAwesomeIcon icon={faJs} />
-        <FontAwesomeIcon icon={faCss3} />
-        <FontAwesomeIcon icon={faAngular} />
-        <FontAwesomeIcon icon={faReact} />
-        <FontAwesomeIcon icon={faJava} />
-        <FontAwesomeIcon icon={faDatabase} />
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faPython} />
+        
+        
         
     </div>
   )
